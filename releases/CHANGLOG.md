c22ac557e9b844632b2db7ad1b341562fde4b873
Chad Keating <me@chadkeating.com>
2021-03-24 16:24:02 +0000

    fixed permission issues when attempting to use the app without upload permission

--- releases/CHANGLOG.md
+++ releases/CHANGLOG.md
@@ -1,1 +1,2 @@
+####Version 1.0.x
 - Prototype integration of Token Stamp 2 with Foundry Prototype Token dialog.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import { canvasReady, ready } from "./hooks";
-
-// Hooks.on("canvasReady", canvasReady);
-// Hooks.on("ready", ready);
-
 const RollAdvantageTokenStamp2 = {
 	getSavePath: () => {
 		return "worlds/" + game.world.name + "/rolladvantage";
@@ -15,14 +10,51 @@ const RollAdvantageTokenStamp2 = {
 			FilePicker.createDirectory("data", savePath)
 		}
 	},
+	//https://stackoverflow.com/questions/50391422/detect-that-given-element-has-been-removed-from-the-dom-without-sacrificing-perf
+	onRemoveHelper: (element, callback) => {
+		const parent = element.parentNode;
+		if (!parent) throw new Error("The node must already be attached");
+
+		const obs = new MutationObserver(mutations => {
+			for (const mutation of mutations) {
+				for (const el of mutation.removedNodes) {
+					if (el === element) {
+						obs.disconnect();
+						callback();
+					}
+				}
+			}
+		});
+		obs.observe(parent, {
+			childList: true,
+		});
+	},
 	render: async (config, html) => {
 		var subHtml = html.find('.tab[data-tab="image"]').find(".form-group .form-fields").first();
 		subHtml.append("<button type='button' id='ra-ts2-open-button' title='Create Token' tabindex='-1'><img src='https://rolladvantage.com/assets/images/logo/default.png' /></button>");
 		subHtml.find("#ra-ts2-open-button").click(event => {
+
+			let isStampOpen = Object.keys(ui.windows)
+				.map(key => ui.windows[key])
+				.some(win => win.options.classes.some(x => x == "ra-ts2-wrapper"));
+
+			if(isStampOpen)
+				return;
+
+			let fp = new FilePicker();
+			if(!fp.canUpload)
+			{
+				ui.notifications.error("You need File Upload permissions to use Token Stamp 2!")
+				return;
+			}
 			event.preventDefault()
 			const wrapper = new TokenStampWrapper();
 			wrapper.render(true);
 			wrapper.exportInputBox = html.find("input.image");
+
+			RollAdvantageTokenStamp2.onRemoveHelper(html[0], ()=>{
+				wrapper.close();
+			});
 		});
 	}
 };
@@ -40,50 +72,23 @@ class TokenStampWrapper extends Application {
 	}
 
 	static createTokenFile(data, filename) {
-		var blobBin = atob(data.split(',')[1]);
-		var array = [];
-		for (var i = 0; i < blobBin.length; i++) {
+		let blobBin = atob(data.split(',')[1]);
+		let array = [];
+		for (let i = 0; i < blobBin.length; i++) {
 			array.push(blobBin.charCodeAt(i));
 		}
-		var fileBlob = new Blob([new Uint8Array(array)], { type: 'image/png' });
+		let fileBlob = new Blob([new Uint8Array(array)], { type: 'image/png' });
 		return new File([fileBlob], filename);
 	}
 
 	static async uploadToken(data, filename) {
-		var file = TokenStampWrapper.createTokenFile(data, filename);
-		return await FilePicker.upload("data", RollAdvantageTokenStamp2.getSavePath(), file, {});
-		/*
-		// Create the form data to post
-		const fd = new FormData();
-		let test = await data;
-		fd.set("source", 'data');
-		fd.set("target", RollAdvantageTokenStamp2.getSavePath());
-		fd.set("upload", test, filename);
-
-		// Dispatch the request
-		const request = await fetch('/upload', { method: "POST", body: fd });
-		if (request.status === 413) {
-			return ui.notifications.error(game.i18n.localize("FILES.ErrorTooLarge"));
-		} else if (request.status !== 200) {
-			return ui.notifications.error(game.i18n.localize("FILES.ErrorSomethingWrong"));
-		}
-
-		// Retrieve the server response
-		const response = await request.json();
-		if (response.error) {
-			ui.notifications.error(response.error);
-			return false;
-		} else if (response.message) {
-			if (/^(modules|systems)/.test(response.path)) {
-				ui.notifications.warn(game.i18n.localize("FILES.WarnUploadModules"))
-			}
-			ui.notifications.info(response.message);
-		}
-		return response;*/
+		let file = TokenStampWrapper.createTokenFile(data, filename);
+		let uploadResponse = await FilePicker.upload("data", RollAdvantageTokenStamp2.getSavePath(), file, {});
+		return uploadResponse;
 	}
 
 	async close() {
-		window.removeEventListener("message", this.foundryImportCallback.bind(this), false);
+		window.removeEventListener("message", this.boundCallback, false);
 		return super.close();
 	}
 
@@ -97,10 +102,11 @@ class TokenStampWrapper extends Application {
 		}
 
 		if (event.data.action == "importToken") {
-			var that = this;
-			var timeStamp = Math.floor(Math.floor(((Date.now() / 100000000) - Math.floor(Date.now() / 100000000)) * 100000000) / 100);
-			var prom = TokenStampWrapper.uploadToken(event.data.stampData, game.userId + "-" + timeStamp + ".png");
-			prom.then(x => {
+			let that = this;
+			let timeStamp = Math.floor(Math.floor(((Date.now() / 100000000) - Math.floor(Date.now() / 100000000)) * 100000000) / 100);
+			let prom = TokenStampWrapper.uploadToken(event.data.stampData, game.userId + "-" + timeStamp + ".png");
+			prom
+			.then(x => {
 				that.exportInputBox[0].value = x.path;
 				that.close();
 			});
@@ -109,12 +115,13 @@ class TokenStampWrapper extends Application {
 
 	activateListeners(html) {
 		super.activateListeners(html);
-		var that = this;
+		let that = this;
 
-		window.addEventListener("message", this.foundryImportCallback.bind(this), false);
+		this.boundCallback = this.foundryImportCallback.bind(this);
+		window.addEventListener("message", this.boundCallback, false);
 
 		TokenStampWrapper.sourceRegistered = false;
-		var iWindow = document.getElementById('ra-ts2-iframe').contentWindow;
+		let iWindow = document.getElementById('ra-ts2-iframe').contentWindow;
 		function waitForTokenStampLoaded() {
 			if(TokenStampWrapper.sourceRegistered) {
 				console.log("Source Registered");
@@ -132,49 +139,6 @@ class TokenStampWrapper extends Application {
 		}
 		waitForTokenStampLoaded();
 	}
-
-	/*
-	 Name 	Type 	Description
-baseApplication 	string
-
-A named "base application" which generates an additional hook
-width 	number
-
-The default pixel width for the rendered HTML
-height 	number
-
-The default pixel height for the rendered HTML
-top 	number
-
-The default offset-top position for the rendered HTML
-left 	number
-
-The default offset-left position for the rendered HTML
-popOut 	boolean
-
-Whether to display the application as a pop-out container
-minimizable 	boolean
-
-Whether the rendered application can be minimized (popOut only)
-resizable 	boolean
-
-Whether the rendered application can be drag-resized (popOut only)
-id 	string
-
-The default CSS id to assign to the rendered HTML
-classes 	Array.<string>
-
-An array of CSS string classes to apply to the rendered HTML
-title 	string
-
-A default window title string (popOut only)
-template 	string
-
-The default HTML template path to render for this Application
-scrollY 	Array.<string>
-
-A list of unique CSS selectors which target containers that should have their vertical scroll positions preserved during a re-render.
-	**/
 }
 
 Hooks.on('ready', () => {
